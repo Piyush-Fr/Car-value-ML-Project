faf27fd9988217f99afda31974b73ee11de993ec
Piyush-Fr <thakurpiyush3112005@gmail.com>
2026-06-09 14:11:44 +0530

    prac

--- feature_engineering.py
+++ feature_engineering.py
@@ -45,3 +45,37 @@ display(audi_df.head(3))
 print(df.info())
 print(df.describe())
 
+
+#Outlier Removal
+
+before = len(df)
+
+df = df[
+    (df['engineSize'] != 0) &
+    (df['price'] >= 500) &
+    (df['price'] <= 100000)
+]
+
+after = len(df)
+
+print(f'Removed {before - after:,} row {before:,} -> {after:,}')
+assert df['engineSize'].min() > 0 
+assert df['price'].min() >= 50
+assert df['price'].max() <= 100000
+print ('Outliers Removed !')
+
+
+#Derive Car Age and Mileage per year
+
+df['car_age'] = 2020 - df['year']
+df['mileage_per_year'] = df['mileage'] / df['car_age']
+
+df['mileage_per_year'] = df['mileage_per_year'].replace([float('inf')], 0)
+df['mileage_per_year'] = df['mileage_per_year'].fillna(0)
+
+assert 'car_age' in df.columns
+assert 'mileage_per_year' in df.columns
+assert df['mileage_per_year'].isna().sum() == 0
+print(f'Car Age {df["car_age"].min():.0f} --> {df["car_age"].max():.0f} ')
+print('Car Age and Mileage Derived')
+
